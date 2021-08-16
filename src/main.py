bbc00dd864fac8eaf5b7f83189a8768df94609f4
Malin <szyszka45@gmail.com>
2021-08-16 17:19:36 +0200

    init

---
+++ src/main.py
@@ -0,0 +1,5 @@
+def hello_world():
+    print("hello world")
+    print(
+        "this line is almost too long, should be about 120 chars ----------------------------------------------------"
+    )
